165c4369907b581fd6f34bd947d15b1f91751a8d
mesutgdk <mesuttgedik@gmail.com>
2023-06-14 15:30:14 +0300

    feat: create new password project

---
+++ Password/Password/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Password
+//
+//  Created by Mesut Gedik on 14.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController ()
+        
+        return true
+    }
+
+}
+
