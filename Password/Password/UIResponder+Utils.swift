4acca5a1ad461ce2a466da880f4076ea309dec63
mesutgdk <mesuttgedik@gmail.com>
2023-06-18 11:53:38 +0300

    feat: add keyboard actions

---
+++ Password/Password/UIResponder+Utils.swift
@@ -0,0 +1,27 @@
+//
+//  UIResponder+Utils.swift
+//  Password
+//
+//  Created by Mesut Gedik on 18.06.2023.
+//
+
+import UIKit
+// it says who the current responder is in view
+extension UIResponder {
+    
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+    
+    //Finds the current first responder
+    // - Returns: the curren UIresponder if it exits
+    static func currentFirst () -> UIResponder? {
+        Static.responder = nil
+        // it fires an event-sendin an action to responder chain, and traping and setting itself
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
